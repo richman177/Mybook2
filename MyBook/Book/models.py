02657198be3d035ceb95c018a88232cc9d382d06
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-07 10:54:00 +0600

    Update models.py
    fddf

--- MyBook/Book/models.py
+++ MyBook/Book/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models 
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models import CharField, ForeignKey
