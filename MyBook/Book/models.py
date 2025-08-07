feb2b7789bb46662c9df130447a005bbc28bdc90
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-07 10:54:18 +0600

    Update models.py
    ghj

--- MyBook/Book/models.py
+++ MyBook/Book/models.py
@@ -1,7 +1,7 @@
 from django.db import models 
 from django.contrib.auth.models import AbstractUser 
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db.models import CharField, ForeignKey
+from django.db.models import CharField, ForeignKey 
 from phonenumber_field.modelfields import PhoneNumberField
 
 STATUS_CHOICES = (
